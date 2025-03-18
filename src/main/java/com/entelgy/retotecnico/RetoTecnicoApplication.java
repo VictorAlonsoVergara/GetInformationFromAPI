23efcecafaa72e88a45a717d329bca5c0016e16f
Victor Alonso Vergara <victor.vh2402@gmail.com>
2025-03-18 14:29:10 -0500

    Subiendo el proyecto completo

---
+++ src/main/java/com/entelgy/retotecnico/RetoTecnicoApplication.java
@@ -0,0 +1,12 @@
+package com.entelgy.retotecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoTecnicoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RetoTecnicoApplication.class, args);
+    }
+
+}
